03f4b6b86c492343b8a40d9c091c98d446d6a177
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-13 16:27:48 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -72,8 +72,8 @@ class TicTacToe
     won = 0
     WIN_COMBINATIONS.each do |combination|
       if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X" || @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
-        return combination 
-        won += 1
+        return combination
+        won = 1
       end
     end
     if won = 0
