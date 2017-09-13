d55b33bc4eef952975e676905b74458c07af3a0f
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-13 16:49:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -112,5 +112,16 @@ class TicTacToe
       return false
     end
   end
-  
+
+  def winner
+    if won?(board)
+    WIN_COMBINATIONS.each do |combination|
+      if @board[combination[0]] == "X" && @board[combination[1]] == "X" && @board[combination[2]] == "X"
+        return "X"
+      elsif @board[combination[0]] == "O" && @board[combination[1]] == "O" && @board[combination[2]] == "O"
+        return "O"
+      end
+    end
+  end
+
 end
